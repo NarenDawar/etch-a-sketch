54d482a3b55feb51dd9a3b676d89e101a30cc11f
NarenDawar <148577200+NarenDawar@users.noreply.github.com>
2023-12-27 22:10:09 -0500

    Rows do not adjust to parent flexbox and rows leave one element out.

--- myscript.js
+++ myscript.js
@@ -1,14 +1,29 @@
 const container = document.querySelector("#container");
+let vars;
 function createGrid(param) {
     for(let i = 0; i < param; i++) {
         for(let j = 0; j < param; j++) {
             const element = document.createElement('div');
-            element.textContent = "Box";
             element.classList.add('box');
             container.appendChild(element);
-        }    
+        }
+        vars = document.createElement('hr');
+        vars.classList.add('hr');
+        container.appendChild(vars);
     }
 }
 
-createGrid(4);
 
+let num_of_pixels = prompt('Enter the dimension you want for the drawing. Max is 100 and the min is 5.');
+while(num_of_pixels > 100 || num_of_pixels < 5) {
+    num_of_pixels = prompt('Enter a number within the guidelines:');
+}
+createGrid(num_of_pixels);
+let boxes = (document.querySelectorAll('.box'));
+boxes.forEach(
+    function(box) {
+        box.addEventListener('mousemove', function(e) {
+            e.target.style.background = 'black';
+        })
+    }
+);
